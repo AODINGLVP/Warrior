9132b0ce25f8a3aa3a1c244efcc8e2d76ba8c0d5
aodinglv-source <aodinglv@163.com>
2026-03-02 18:01:40 +0000

    control

--- Source/Warrior/Public/Characters/WarriorHeroCharacter.h
+++ Source/Warrior/Public/Characters/WarriorHeroCharacter.h
@@ -5,7 +5,8 @@
 #include "CoreMinimal.h"
 #include "Characters/WarriorBaseCharacter.h"
 #include "WarriorHeroCharacter.generated.h"
-
+class USpringArmComponent;
+class UCameraComponent;
 /**
  * 
  */
@@ -13,5 +14,14 @@ UCLASS()
 class WARRIOR_API AWarriorHeroCharacter : public AWarriorBaseCharacter
 {
 	GENERATED_BODY()
-	
+public:
+	AWarriorHeroCharacter();
+protected:
+	virtual void BeginPlay() override;
+private:
+#pragma region Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FollowCamera;
 };
---
+++ Source/Warrior/Public/DataAssets/Input/DataAsset_InputConfig.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
+#include "DataAsset_InputConfig.generated.h"
+
+class UInputAction;
+class UInputMappingContext;
+
+USTRUCT(BlueprintType)
+struct FWarriorInputActionConfig
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, meta = (Categories="InputTag"))
+	FGameplayTag InputTag;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputAction* InputAction;
+};
+/**
+ * 
+ */
+UCLASS()
+class WARRIOR_API UDataAsset_InputConfig : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly)
+	UInputMappingContext* DefultMappingContext;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly,meta=(TitleProerty="InputTag"))
+	TArray<FWarriorInputActionConfig> NativeInputActions;
+	UInputAction* FindNativeInputActionByTag(const FGameplayTag& InInputTag);
+	
+};
